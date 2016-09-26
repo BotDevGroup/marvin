f6fd45d8d486b4acd3c38cdc38601f6e37e9baa0
Ricardo Arturo Cabral Mejía <ricardo.arturo.cabral@gmail.com>
2016-09-25 21:52:31 -0400

    Implemented message views with the following commands: /info /error /warning.

--- bot.py
+++ bot.py
@@ -1,13 +1,15 @@
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackQueryHandler
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from telegram.ext import CallbackQueryHandler
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 from app.config import config
+from app.views import WarningView, ErrorView, InfoView
+
 import logging
 
 # Enable logging
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+logging.basicConfig(format='%(asctime)s [%(name)s %(levelname)s]: %(message)s',
                     level=logging.INFO)
 logger = logging.getLogger(__name__)
-
 
 
 def start(bot, update):
@@ -24,8 +26,10 @@ def start(bot, update):
 def echo(bot, update):
     update.message.reply_text(update.message.text)
 
+
 def ping(bot, update):
     update.message.reply_text("pong")
+
 
 def button(bot, update):
     query = update.callback_query
@@ -35,13 +39,30 @@ def button(bot, update):
                         message_id=query.message.message_id)
 
 
+def info(bot, update):
+    msg = InfoView("this is a test")
+    # print(str(update).encode('utf8'))
+    # update.message.reply_text("info")
+    update.message.reply_text(str(msg))
+
+
+def errorm(bot, update):
+    msg = ErrorView("this is a test")
+    update.message.reply_text(str(msg))
+
+
+def warning(bot, update):
+    msg = WarningView("this is a test")
+    update.message.reply_text(str(msg))
+
+
 def help(bot, update):
-    update.message.reply_text("Use /start to test this bot. Other commands: /echo, /ping")
+    update.message.reply_text(
+        "Use /start to test this bot. Other commands: /echo, /ping")
 
 
 def error(bot, update, error):
     logging.warning('Update "%s" caused error "%s"' % (update, error))
-
 
 # Create the Updater and pass it your bot's token.
 updater = Updater(config.config["Debug"]["TelegramBotToken"])
@@ -51,6 +72,9 @@ updater.dispatcher.add_handler(CallbackQueryHandler(button))
 updater.dispatcher.add_handler(CommandHandler('help', help))
 updater.dispatcher.add_handler(CommandHandler('echo', echo))
 updater.dispatcher.add_handler(CommandHandler('ping', ping))
+updater.dispatcher.add_handler(CommandHandler('info', info))
+updater.dispatcher.add_handler(CommandHandler('warning', warning))
+updater.dispatcher.add_handler(CommandHandler('error', errorm))
 updater.dispatcher.add_error_handler(error)
 
 # Start the Bot
