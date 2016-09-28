2034ed29917cd114064ff7d57df233b5d95ff35e
Ricardo Restituyo <ricardo@codemera.com>
2016-09-28 09:47:22 -0400

    Ensure only one `fetch_messages` can run at a given time

--- marvinbot/tasks.py
+++ marvinbot/tasks.py
@@ -8,18 +8,33 @@ from telegram.error import NetworkError, Unauthorized
 log = get_task_logger(__name__)
 adapter = make_adapter()
 
+LOCK_EXPIRE = 60*5
+
 
 @marvinbot_app.task(bind=True)
 def fetch_messages(self):
-    try:
-        for update in adapter.fetch_updates(cache.get('last_update_id', 30)):
-            adapter.process_update(update)
-            cache.set('last_update_id', update.update_id + 1)
-    except NetworkError:
-        log.info("No more updates to fetch")
-    except Unauthorized:
-        # The user has removed or blocked the bot.
-        log.error("Unauthorized: User might have blocked a bot")
+    lock_id = '{0}-lock'.format(self.name)
+
+    # cache.add fails if the key already exists
+    acquire_lock = lambda: cache.get_or_create(lock_id, lambda: True, LOCK_EXPIRE)
+    # memcache delete is very slow, but we have to use it to take
+    # advantage of using add() for atomic locking
+    release_lock = lambda: cache.delete(lock_id)
+
+    if acquire_lock():
+        try:
+            for update in adapter.fetch_updates(cache.get('last_update_id', ignore_expiration=True)):
+                adapter.process_update(update)
+                cache.set('last_update_id', update.update_id + 1)
+        except NetworkError:
+            log.info("No more updates to fetch")
+        except Unauthorized:
+            # The user has removed or blocked the bot.
+            log.error("Unauthorized: User might have blocked a bot")
+        finally:
+            release_lock()
+    else:
+        log.info("Another fetch_message operation is already running")
 
 
 @adapter.register_command('start')
