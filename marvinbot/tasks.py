8de242bace6504c0c1eb2c5d11c2237a27dd88a5
Ricardo Restituyo <ricardo@codemera.com>
2016-09-30 23:45:44 -0400

    Notify owners when bot starts or shutsdown

--- marvinbot/tasks.py
+++ marvinbot/tasks.py
@@ -1,11 +1,19 @@
 from marvinbot.celeryapp import marvinbot_app
 from marvinbot.core import get_adapter
 from celery.utils.log import get_task_logger
+from marvinbot.signals import bot_started, bot_shutdown
+from marvinbot.models import User
 
 
 log = get_task_logger(__name__)
 adapter = get_adapter()
 
 
+@bot_started.connect
+def on_start(adapter):
+    adapter.notify_owners('✅ *Bot started*')
 
 
+@bot_shutdown.connect
+def on_shutdown(adapter):
+    adapter.notify_owners('❌ *Bot shutting down*')
