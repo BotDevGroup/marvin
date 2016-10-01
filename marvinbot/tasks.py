4541fc71870c66ceabbaf25367607329a4dfee30
Ricardo Restituyo <ricardo@codemera.com>
2016-10-01 11:08:19 -0400

    Add /test command

--- marvinbot/tasks.py
+++ marvinbot/tasks.py
@@ -3,6 +3,7 @@ from marvinbot.core import get_adapter
 from celery.utils.log import get_task_logger
 from marvinbot.signals import bot_started, bot_shutdown
 from marvinbot.models import User
+from marvinbot.handlers import CommandHandler, Filters
 
 
 log = get_task_logger(__name__)
@@ -17,3 +18,11 @@ def on_start(adapter):
 @bot_shutdown.connect
 def on_shutdown(adapter):
     adapter.notify_owners('❌ *Bot shutting down*')
+
+
+@marvinbot_app.task
+def you_there(update, *args, **kwargs):
+    update.message.reply_text('Me here 👀, are _you_?', parse_mode='Markdown')
+
+
+adapter.add_handler(CommandHandler('test', you_there, call_async=False))
